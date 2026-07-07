199557f3353f9eaeb05081ffcc6c2118b6e91eaa
Yingka10 <jennychou0710@gmail.com>
2026-07-07 23:08:44 +0800

    套件bug修正

--- babel.config.js
+++ babel.config.js
@@ -5,6 +5,9 @@ module.exports = function(api) {
 
   return {
     presets: [expoPreset],
-    plugins: ['react-native-reanimated/plugin'],
+    // Reanimated/Worklets 的 babel plugin 由 babel-preset-expo (SDK 54) 偵測到
+    // react-native-worklets 後自動加入。手動再加 'react-native-reanimated/plugin'
+    // 會讓同一個 worklets plugin 被套用兩次，手機端 bundle 壞掉、Expo Go 打不開。
+    // 不要加回來。
   };
 };
